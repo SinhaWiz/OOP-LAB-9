fd2126915b37876c7534681a73052a53dcaa4632
SinhaWiz <sinhartx2070@gmail.com>
2025-02-18 11:37:36 +0600

    TEST cases added

---
+++ src/test/OrderTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void calculateSubtotal() {
+    }
+
+    @Test
+    void calculateTax() {
+    }
+
+    @Test
+    void calculateTotal() {
+    }
+}
---
+++ src/test/YogurtFlavorTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class YogurtFlavorTest {
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getUnit() {
+    }
+}
